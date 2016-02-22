3839c088da35a75992b3221c8d322b421e13ca2d
melkinsgit <margaret.elkins@gmail.com>
2016-02-22 17:50:45 -0600

    Working on Adv Pt01

---
+++ M Elkins Lab 06 Pt01 Adv JS.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Margaret on 2/22/2016.
+ */
+$('document').ready(function() {
+    // http://stackoverflow.com/questions/23289944/jquery-event-handler-for-input-type-text
+    $('#btnAddToList').on('click',function (){
+        addItem();
+    });
+});
+
+function addItem () {
+    var statement = $('input').val();
+    $('input').val('');
+
+    var theList = $('#listTitle');
+    var $newListItem =$('<li class="listElement"></li>');
+    $newListItem.append(statement);
+    //$newListItem.addClass('notDone');
+    $('.listElement').click(function() {
+        $(this).toggleClass('done');
+    });
+
+    theList.append($newListItem);
+}
